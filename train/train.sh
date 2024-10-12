3339cd49240c29ff7b75d03f256614f1f6b32f7c
nuoline <nuoline@gmail.com>
2024-10-12 13:54:11 +0800

    Update train.sh

--- train/train.sh
+++ train/train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=../nseg_model/ncrfpp/lib/:$LD_LIBRARY_PATH
-../ncrfpp/bin/crf_learn -f 3 -c 4.0 seg.template data/scw.train.tag nseg.model
+../nseg_model/ncrfpp/bin/crf_learn -f 3 -c 4.0 seg.template data/scw.train.tag nseg.model
