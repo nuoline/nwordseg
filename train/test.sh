fd31fb42f47de9be25d28874c2854aa297b49e1e
nuoline <nuoline@gmail.com>
2024-10-12 13:53:52 +0800

    Update test.sh

--- train/test.sh
+++ train/test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-../ncrfpp/bin/crf_test -m nseg.model data/scw.test.tag -o data/scw.test.tag_res.txt 
+export LD_LIBRARY_PATH=../nseg_model/ncrfpp/lib/:$LD_LIBRARY_PATH
+../nseg_model/ncrfpp/bin/crf_test -m nseg.model data/scw.test.tag -o data/scw.test.tag_res.txt 
